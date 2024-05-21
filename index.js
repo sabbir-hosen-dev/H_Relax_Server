3ed69b0ec74210331a3afc6689240c5c7cbafe80
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-05-21 11:56:43 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const PORT = 5050;
 
@@ -11,7 +12,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
