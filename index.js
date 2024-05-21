e8eefe17c9ae0f761155ea6bf7bc9c7454371fd8
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-05-21 11:22:24 +0600

    verel add

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('dotenv').config()
+require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@cluster0.02y3257.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://h-relax:${process.env.DB_PASS}@cluster0.02y3257.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,11 +27,12 @@ const client = new MongoClient(uri, {
 
 const admin = require("firebase-admin");
 
-var serviceAccount = require("./config/h-realax-firebase-adminsdk-iq9ac-5affb14f65.json");
 const { getAuth } = require("firebase-admin/auth");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    require("./config/h-realax-firebase-adminsdk-iq9ac-e4b369cf41.json")
+  ),
 });
 
 async function run() {
@@ -39,21 +40,26 @@ async function run() {
     await client.connect();
     console.log("Connectted To MOngodb");
 
-    const collaction = client.db(process.env.DB_NAME).collection(process.env.DB_COLLACTION);
+    const collaction = client
+      .db( process.env.DB_NAME)
+      .collection(process.env.DB_COLLECTION);
 
     app.get("/bookdin", async (req, res) => {
       const email = req.query.email;
       const bearer = req.headers.authorization;
-    
+
       if (bearer && bearer.startsWith("Bearer ")) {
+        console.log("brr get");
         const idToken = bearer.split(" ")[1];
         try {
           const decodedToken = await getAuth().verifyIdToken(idToken);
           const tokenEmail = decodedToken.email;
           if (email === tokenEmail) {
+            console.log("email get");
             const data = await collaction.find({ userEmail: email }).toArray();
             res.send(data);
           } else {
+            console.log("data not");
             res.status(403).send("Unauthorized");
           }
         } catch (error) {
@@ -64,12 +70,12 @@ async function run() {
         res.status(401).send("Unauthorized");
       }
     });
-    
 
     //post data mongodb
     app.post("/addData", async (req, res) => {
       try {
         const data = req.body;
+        console.log(req.body);
         const result = await collaction.insertOne(data);
         res.send(result);
       } catch (err) {
@@ -77,12 +83,10 @@ async function run() {
       }
     });
 
-    app.get("/", (req, res) => {
-      res.send("hellow iam server");
-    });
-
     app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
+      console.log(
+        `${process.env.FIRE_BASE_ADIMN} Server is running at http://localhost:${PORT}`
+      );
     });
   } finally {
     // Ensures that the client will close when you finish/error
